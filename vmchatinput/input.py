b963947925e973249bfbea79e06c9bdc04f17f30
Christopher Foo <chris.foo@gmail.com>
2015-08-11 01:45:44 -0400

    input: Tweak random for mouse center. Tweak mouse delta to min 0.

--- vmchatinput/input.py
+++ vmchatinput/input.py
@@ -255,7 +255,7 @@ class ChatInput(object):
 
     def _process_as_mouse_input(self, chat_data):
         first_word = chat_data.first_word
-        delta = int(self._random.uniform(10, MAX_MOUSE_MOVE_AMOUNT))
+        delta = int(self._random.uniform(0, MAX_MOUSE_MOVE_AMOUNT))
         delta_x = 0
         delta_y = 0
         left_click = False
@@ -295,7 +295,7 @@ class ChatInput(object):
             elif bet_team == 'red':
                 delta_y = delta
 
-        elif self._random.random() < 0.05:
+        elif self._random.random() < 0.01:
             center = True
 
         else:
